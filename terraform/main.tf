d9795b4d2ac996c6d20c6d881f14c0c02eccb83d
Craig Luedtke <craigluedtke@gmail.com>
2024-07-21 10:25:53 -0500

    make profile variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,15 @@
 provider "aws" {
-  profile = "free"
+  profile = var.aws_profile
   region  = "us-east-1"
 }
 
 locals {
   app_name = "once-human-codes"
+}
+
+variable "aws_profile" {
+  description = "AWS profile to use"
+  default     = "default"
 }
 
 variable "discord_webhook" {
